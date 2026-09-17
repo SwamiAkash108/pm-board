b43afa51e833504a526784138b7f7de4b2dc6313
SwamiAkash108 <akashananda@bhaktimarga.org>
2026-09-17 19:31:22 +0200

    Bump cache to akasha-v22

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "akasha-v21";
+const CACHE = "akasha-v22";
 const SHELL = ["./", "index.html", "styles.css", "app.js", "db.js", "config.js", "manifest.webmanifest", "icon-192.png", "icon-512.png", "bm-logo-black.png"];
 
 self.addEventListener("install", (e) => {
